48579eba770922d8f31f07c52a3c4d0aaf6d5f73
Tsubasa Nakayama <app.nakayama@gmail.com>
2021-12-04 17:42:39 +0900

    use gson

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,13 +1,19 @@
 package app;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
   public static void main(String[] args) {
-    Gson gson = new Gson();
     User user = new User(100, "User1");
-    System.out.println(gson.toJson(user));
     System.out.println(user.getId());
     System.out.println(user.getName());
+    String src = "{\"id\":123,\"name\":\"foo\"}";
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+        System.out.println(mapper.readValue(src, User.class).getName());
+    } catch(JsonProcessingException e) {
+        System.out.println("Error: " + e.getLocalizedMessage());
+    }
   }
 }
--- src/main/java/app/User.java
+++ src/main/java/app/User.java
@@ -1,11 +1,15 @@
 package app;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
-class User {
+class User implements Serializable{
   private int id;
   private String name;
 }
