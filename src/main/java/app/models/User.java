551c323a4150bef3b6e05a3db21ffebf098de2a0
Tsubasa Nakayama <app.nakayama@gmail.com>
2021-12-05 00:16:16 +0900

    fix

--- src/main/java/app/models/User.java
+++ src/main/java/app/models/User.java
@@ -2,6 +2,8 @@ package app.models;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class User implements Serializable{
   private int id;
   private String name;
--- src/main/java/app/tasks/JsonTask.java
+++ src/main/java/app/tasks/JsonTask.java
@@ -7,12 +7,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JsonTask implements Task {
     @Override
     public void run() {
-        String src = "{\"id\":123,\"name\":\"foo\"}";
+        String src = "{\"id\":123,\"name\":\"foo\",\"message\":\"hello\"}";
+        User user = new User(456, "abc");
         ObjectMapper mapper = new ObjectMapper();
         try {
             System.out.println(mapper.readValue(src, User.class).getName());
+            System.out.println(mapper.writeValueAsString(user));
         } catch(JsonProcessingException e) {
             System.out.println("Error: " + e.getLocalizedMessage());
         }
     }
+
+
 }
