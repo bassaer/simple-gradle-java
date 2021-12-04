7ce5a27ee24b8962b8ebaaf4a22ddafea84ce8d2
Tsubasa Nakayama <app.nakayama@gmail.com>
2021-12-05 00:17:38 +0900

    fix

--- src/main/java/app/models/User.java
+++ src/main/java/app/models/User.java
@@ -1,12 +1,11 @@
 package app.models;
 
-import java.io.Serializable;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
